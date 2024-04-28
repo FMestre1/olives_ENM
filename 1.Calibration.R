907927efcdbabf8eac89e6b867b73c920ff5aa6e
FMestre1 <mestre.frederico@gmail.com>
2024-04-28 21:30:29 +0100

    28-04-2024

--- 1.Calibration.R
+++ 1.Calibration.R
@@ -2,9 +2,7 @@
 #30/07/2021
 
 #6. Build Models
-M_olive_W <- sdm::sdm(occ~.,data=data_olive_W,methods=c('cart','rf','fda','glm','gam','mars','svm','brt'),replication='sub',test.percent=30,n=10)
 M_olive_D <- sdm::sdm(occ~.,data=data_olive_D,methods=c('cart','rf','fda','glm','gam','mars','svm','brt'),replication='sub',test.percent=30,n=10)
-M_olive_WD <- sdm::sdm(occ~.,data=data_olive_WD,methods=c('cart','rf','fda','glm','gam','mars','svm','brt'),replication='sub',test.percent=30,n=10)
 #
 
 # 7.Model metrics and variable importance
@@ -16,20 +14,6 @@ rcurve(M_olive_D, id=1:80,n =names(preds2)[6:10], mean=TRUE, smooth=TRUE)
 ev_metrics_olive_D <- getEvaluation(M_olive_D,stat=c('TSS','Kappa','AUC', 'specificity', 'sensitivity'),opt="max(se+sp)")
 mean_ev_metrics_olive_D <- as.data.frame(round(colMeans(x=ev_metrics_olive_D),3))
 #
-#getModelInfo(M_olive_W)
-plot(getVarImp(M_olive_W),'auc')
-rcurve(M_olive_W, id=1:80,n =names(preds2)[1:5], mean=TRUE, smooth=TRUE)
-rcurve(M_olive_W, id=1:80,n =names(preds2)[6:10], mean=TRUE, smooth=TRUE)
-ev_metrics_olive_W <- getEvaluation(M_olive_W,stat=c('TSS','Kappa','AUC', 'specificity', 'sensitivity'),opt="max(se+sp)")
-mean_ev_metrics_olive_W <- as.data.frame(round(colMeans(x=ev_metrics_olive_W),3))
-#
-#getModelInfo(M_olive_WD)
-plot(getVarImp(M_olive_WD),'auc')
-rcurve(M_olive_WD, id=1:80,n =names(preds2)[1:5], mean=TRUE, smooth=TRUE)
-rcurve(M_olive_WD, id=1:80,n =names(preds2)[6:10], mean=TRUE, smooth=TRUE)
-ev_metrics_olive_WD <- getEvaluation(M_olive_WD,stat=c('TSS','Kappa','AUC', 'specificity', 'sensitivity'),opt="max(se+sp)")
-mean_ev_metrics_olive_WD <- as.data.frame(round(colMeans(x=ev_metrics_olive_WD),3))
-
 save.image("olives.RData")
 #load("olives.RData")
 
--- 3.Projection.R
+++ 3.Projection.R
@@ -3,19 +3,11 @@
 
 #10. Build ensemble
 ensemble_olive_D <- sdm::ensemble(M_olive_D,newdata=high_res_preds,filename='ensemble_olive_D_current.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W <- sdm::ensemble(M_olive_W,newdata=high_res_preds,filename='ensemble_olive_W_current.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD <- sdm::ensemble(M_olive_WD,newdata=high_res_preds,filename='ensemble_olive_WD_current.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #plot(ensemble_olive_D)
-#plot(ensemble_olive_W)
-#plot(ensemble_olive_WD)
 
 #Metrics
 metrics_olive_D <- evaluates(data_olive_D, ensemble_olive_D)
 metrics_olive_D@statistics$AUC
-metrics_olive_W <- evaluates(data_olive_W, ensemble_olive_W)
-metrics_olive_W@statistics$AUC
-metrics_olive_WD <- evaluates(data_olive_WD, ensemble_olive_WD)
-metrics_olive_WD@statistics$AUC
 #
 #Clean environment
 #rm(high_res_preds)
@@ -29,14 +21,10 @@ metrics_olive_WD@statistics$AUC
 
 ########## RCP45 ########## 
 ensemble_olive_D_2050_CC_45 <- sdm::ensemble(M_olive_D,newdata=CC_rcp45_vars,filename='ensemble_olive_D_2050_CC_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_CC_45 <- sdm::ensemble(M_olive_W,newdata=CC_rcp45_vars,filename='ensemble_olive_W_2050_CC_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_CC_45 <- sdm::ensemble(M_olive_WD,newdata=CC_rcp45_vars,filename='ensemble_olive_WD_2050_CC_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(CC_rcp45_vars)
 
 ########## RCP85 ########## 
 ensemble_olive_D_2050_CC_85 <- sdm::ensemble(M_olive_D,newdata=CC_rcp85_vars,filename='ensemble_olive_D_2050_CC_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_CC_85 <- sdm::ensemble(M_olive_W,newdata=CC_rcp85_vars,filename='ensemble_olive_W_2050_CC_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_CC_85 <- sdm::ensemble(M_olive_WD,newdata=CC_rcp85_vars,filename='ensemble_olive_WD_2050_CC_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(CC_rcp85_vars)
 
 ######################## CNRM-CM5 (CN) ########################
@@ -50,72 +38,51 @@ ensemble_olive_WD_2050_CC_85 <- sdm::ensemble(M_olive_WD,newdata=CC_rcp85_vars,f
 
 ########## RCP45 ########## 
 ensemble_olive_D_2050_GF_45 <- sdm::ensemble(M_olive_D,newdata=GF_rcp45_vars,filename='ensemble_olive_D_2050_GF_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_GF_45 <- sdm::ensemble(M_olive_W,newdata=GF_rcp45_vars,filename='ensemble_olive_W_2050_GF_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_GF_45 <- sdm::ensemble(M_olive_WD,newdata=GF_rcp45_vars,filename='ensemble_olive_WD_2050_GF_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(GF_rcp45_vars)
 
 ########## RCP85 ########## 
 ensemble_olive_D_2050_GF_85 <- sdm::ensemble(M_olive_D,newdata=GF_rcp85_vars,filename='ensemble_olive_D_2050_GF_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_GF_85 <- sdm::ensemble(M_olive_W,newdata=GF_rcp85_vars,filename='ensemble_olive_W_2050_GF_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_GF_85 <- sdm::ensemble(M_olive_WD,newdata=GF_rcp85_vars,filename='ensemble_olive_WD_2050_GF_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(GF_rcp85_vars)
 
 ######################## HadGEM2-ES	(HE) ######################## 
 
 ########## RCP45 ########## 
 ensemble_olive_D_2050_HE_45 <- sdm::ensemble(M_olive_D,newdata=HE_rcp45_vars,filename='ensemble_olive_D_2050_HE_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_HE_45 <- sdm::ensemble(M_olive_W,newdata=HE_rcp45_vars,filename='ensemble_olive_W_2050_HE_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_HE_45 <- sdm::ensemble(M_olive_WD,newdata=HE_rcp45_vars,filename='ensemble_olive_WD_2050_HE_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(HE_rcp45_vars)
 
 ########## RCP85 ########## 
 ensemble_olive_D_2050_HE_85 <- sdm::ensemble(M_olive_D,newdata=HE_rcp85_vars,filename='ensemble_olive_D_2050_HE_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_HE_85 <- sdm::ensemble(M_olive_W,newdata=HE_rcp85_vars,filename='ensemble_olive_W_2050_HE_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_HE_85 <- sdm::ensemble(M_olive_WD,newdata=HE_rcp85_vars,filename='ensemble_olive_WD_2050_HE_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(HE_rcp85_vars)
 
 ######################## INMCM4 (IN) ######################## 
 
 ########## RCP45 ########## 
 ensemble_olive_D_2050_IN_45 <- sdm::ensemble(M_olive_D,newdata=IN_rcp45_vars,filename='ensemble_olive_D_2050_IN_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_IN_45 <- sdm::ensemble(M_olive_W,newdata=IN_rcp45_vars,filename='ensemble_olive_W_2050_IN_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_IN_45 <- sdm::ensemble(M_olive_WD,newdata=IN_rcp45_vars,filename='ensemble_olive_WD_2050_IN_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(IN_rcp45_vars)
 
 ########## RCP85 ########## 
 ensemble_olive_D_2050_IN_85 <- sdm::ensemble(M_olive_D,newdata=IN_rcp85_vars,filename='ensemble_olive_D_2050_IN_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_IN_85 <- sdm::ensemble(M_olive_W,newdata=IN_rcp85_vars,filename='ensemble_olive_W_2050_IN_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_IN_85 <- sdm::ensemble(M_olive_WD,newdata=IN_rcp85_vars,filename='ensemble_olive_WD_2050_IN_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(IN_rcp85_vars)
 
 ######################## IPSL-CM5A-LR	(IP) ######################## 
 
 ########## RCP45 ########## 
 ensemble_olive_D_2050_IP_45 <- sdm::ensemble(M_olive_D,newdata=IP_rcp45_vars,filename='ensemble_olive_D_2050_IP_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_IP_45 <- sdm::ensemble(M_olive_W,newdata=IP_rcp45_vars,filename='ensemble_olive_W_2050_IP_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_IP_45 <- sdm::ensemble(M_olive_WD,newdata=IP_rcp45_vars,filename='ensemble_olive_WD_2050_IP_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(IP_rcp45_vars)
 
 ########## RCP85 ########## 
 ensemble_olive_D_2050_IP_85 <- sdm::ensemble(M_olive_D,newdata=IP_rcp85_vars,filename='ensemble_olive_D_2050_IP_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_IP_85 <- sdm::ensemble(M_olive_W,newdata=IP_rcp85_vars,filename='ensemble_olive_W_2050_IP_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_IP_85 <- sdm::ensemble(M_olive_WD,newdata=IP_rcp85_vars,filename='ensemble_olive_WD_2050_IP_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(IP_rcp85_vars)
 
 ######################## MPI-ESM-LR	(MP) ######################## 
 
 ########## RCP45 ########## 
 ensemble_olive_D_2050_MP_45 <- sdm::ensemble(M_olive_D,newdata=MP_rcp45_vars,filename='ensemble_olive_D_2050_MP_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_MP_45 <- sdm::ensemble(M_olive_W,newdata=MP_rcp45_vars,filename='ensemble_olive_W_2050_MP_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_MP_45 <- sdm::ensemble(M_olive_WD,newdata=MP_rcp45_vars,filename='ensemble_olive_WD_2050_MP_45.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(MP_rcp45_vars)
 
 ########## RCP85 ########## 
 ensemble_olive_D_2050_MP_85 <- sdm::ensemble(M_olive_D,newdata=MP_rcp85_vars,filename='ensemble_olive_D_2050_MP_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_W_2050_MP_85 <- sdm::ensemble(M_olive_W,newdata=MP_rcp85_vars,filename='ensemble_olive_W_2050_MP_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
-ensemble_olive_WD_2050_MP_85 <- sdm::ensemble(M_olive_WD,newdata=MP_rcp85_vars,filename='ensemble_olive_WD_2050_MP_85.img',setting=list(method='weighted',stat='TSS',power=2),overwrite=TRUE) 
 #rm(MP_rcp85_vars)
-
 
 ############################################################ 
 save.image("olives.RData")
